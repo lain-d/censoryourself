2701abfe3196fe1939040a09707d87950e150294
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2016-06-06 15:06:31 -0400

    started JS
    changed the file upload to be “input” instead of a button, #important

--- js/site.js
+++ js/site.js
@@ -1,1 +1,21 @@
 //Site Script
+$( document ).ready(function() {
+ 
+
+var c = document.getElementById("maincanvas");
+var ctx = c.getContext("2d");
+
+
+$("#urlSubmit").on("click", function(){
+
+var img = new Image();
+img.onload = function() {
+    ctx.drawImage(img, 0, 0);
+};
+img.src = $("#imageURL").val();
+
+});
+
+
+
+});
