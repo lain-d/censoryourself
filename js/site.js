c9e01f0aa0309708f74f3c785665d225f1435970
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2016-06-07 18:05:24 -0400

    javascript fixes
    All JS functionality is done, just need to add UX tweaks once design is
    ready

--- js/site.js
+++ js/site.js
@@ -1,63 +1,61 @@
 //Site Script
 
+//Globals
+var c;
+var ctx;
+var overlay = new Array("images/ContentCensored.jpg", "images/GlobalAlly.jpg");
+
+//Helper Functions
 function downloadCanvas(link, canvasId, filename) {
     link.href = document.getElementById(canvasId).toDataURL();
     link.download = filename;
 }
-
-
-
-function handleImage(e){
+function handleImage(e) {
     var reader = new FileReader();
-    reader.onload = function(event){
+    reader.onload = function(event) {
         var img = new Image();
-        img.onload = function(){
-            ctx.drawImage(img,0,0, 800, 800);
-        //    
-            img2.src = "images/GlobalAlly.jpg";
+        img.setAttribute('crossOrigin', 'anonymous');
+        $(img).error(function() { console.log("Sorry, the URL provided doesn't work"); });
+        img.onload = function() {
+            ctx.clearRect(0, 0, c.width, c.height);
+            ctx.drawImage(img, 0, 0, 800, 800);
+
+            img2.src = overlay[$("#otype").find('input').index($("#otype").find(':checked'))];
         };
         img.src = event.target.result;
-        img.setAttribute('crossorigin', 'anonymous');
     };
-    reader.readAsDataURL(e.target.files[0]);     
+    reader.readAsDataURL(e.target.files[0]);
 }
 
-var c;
-var ctx;
-var img2 = new Image();
+//Page Load
+$(document).ready(function() {
 
+    c = document.getElementById("maincanvas");
+    ctx = c.getContext("2d");
 
-$( document ).ready(function() {
-c = document.getElementById("maincanvas");
-ctx = c.getContext("2d");
+    img2.onload = function() {
+        ctx.drawImage(img2, 0, 250);
+        $("#output").empty();
+        $("#output").append('<img src="' + c.toDataURL() + '">');
+    };
+    var imageLoader = document.getElementById('fileupload');
+    imageLoader.addEventListener('change', handleImage, false);
+    $("#urlSubmit").on("click", function() {
+        event.preventDefault();
+        var img = new Image();
+        img.setAttribute('crossOrigin', 'anonymous');
+        $(img).error(function() { console.log("Sorry, the URL provided doesn't work"); });
+        img.onload = function() {
+            ctx.clearRect(0, 0, c.width, c.height);
+            ctx.drawImage(img, 0, 0, 800, 800);
+            img2.src = overlay[$("#otype").find('input').index($("#otype").find(':checked'))];
+        };
 
-img2.onload = function() {
-    ctx.drawImage(img2, 0, 250);
-      $("#output").append('<img src="'+c.toDataURL()+'">');
-};
+        img.src = $("#imageURL").val();
+    });
 
-var imageLoader = document.getElementById('fileupload');
-imageLoader.addEventListener('change', handleImage, false);
-
-
-
-
-$("#urlSubmit").on("click", function(){
-    event.preventDefault();
-
-var img = new Image();
-img.onload = function() {
-    ctx.drawImage(img, 0, 0, 800, 800);
- //   img2.setAttribute('crossOrigin', 'anonymous');
-    img2.src = "images/GlobalAlly.jpg";
-};
-img.setAttribute('crossOrigin', 'anonymous');
-img.src = $("#imageURL").val();
-});
-
-
-$("#dlImage").on("click", function(){
-  downloadCanvas(this, 'maincanvas', 'globalAlly.png');
-});
+    $("#dlImage").on("click", function() {
+        downloadCanvas(this, 'maincanvas', 'globalAlly.png');
+    });
 
 });
