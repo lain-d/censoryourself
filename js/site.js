b9d14c2efa4f57893c7d7bfd83c77135d5474585
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2016-06-07 10:48:32 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 161272d6c313b457f7d66f07ea60b77260fad7a8, reversing
    changes made to 88cf5948f4a5dd596a5a129ef21823cc83576160.

--- js/site.js
+++ js/site.js
@@ -1,64 +1,21 @@
 //Site Script
-
-function downloadCanvas(link, canvasId, filename) {
-    link.href = document.getElementById(canvasId).toDataURL();
-    link.download = filename;
-}
-
-
-
-function handleImage(e){
-    var reader = new FileReader();
-    reader.onload = function(event){
-        var img = new Image();
-        img.onload = function(){
-            ctx.drawImage(img,0,0, 800, 800);
-        //    
-            img2.src = "images/GlobalAlly.jpg";
-        };
-        img.src = event.target.result;
-        img.setAttribute('crossorigin', 'anonymous');
-    };
-    reader.readAsDataURL(e.target.files[0]);     
-}
-
-var c;
-var ctx;
-var img2 = new Image();
-
-
 $( document ).ready(function() {
-c = document.getElementById("maincanvas");
-ctx = c.getContext("2d");
+ 
 
-img2.onload = function() {
-    ctx.drawImage(img2, 0, 250);
-      $("#output").append('<img src="'+c.toDataURL()+'">');
-};
-
-var imageLoader = document.getElementById('fileupload');
-imageLoader.addEventListener('change', handleImage, false);
-
-
+var c = document.getElementById("maincanvas");
+var ctx = c.getContext("2d");
 
 
 $("#urlSubmit").on("click", function(){
-    event.preventDefault();
 
 var img = new Image();
 img.onload = function() {
-    ctx.drawImage(img, 0, 0, 800, 800);
- //   img2.setAttribute('crossOrigin', 'anonymous');
-    img2.src = "images/GlobalAlly.jpg";
+    ctx.drawImage(img, 0, 0);
 };
 img.src = $("#imageURL").val();
-img.setAttribute('crossorigin', 'anonymous');
 
 });
 
 
-$("#dlImage").on("click", function(){
-  downloadCanvas(this, 'maincanvas', 'globalAlly.png');
-});
 
 });
