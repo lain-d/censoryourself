f17b8875c7ac868a0a85cc5d321e11a0100e83a7
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2016-06-08 17:36:43 -0400

    clean up
    Removed unused Javascript
    Minified Javascript

--- js/site.js
+++ js/site.js
@@ -1,5 +1,4 @@
 //Site Script
-
 //Globals
 var c;
 var ctx;
@@ -8,19 +7,16 @@ var img2 = new Image();
 var img = new Image();
 var imgloaded = false;
 
-
-function isUrlValid(url) {
-    return /^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(url);
-}
 //Helper Functions
 function downloadCanvas(link, canvasId, filename) {
     link.href = document.getElementById(canvasId).toDataURL();
     link.download = filename;
 }
-function handleImage(e) {
+
+function loadImage(e) {
     var reader = new FileReader();
     reader.onload = function(event) {
-    
+
         img.setAttribute('crossOrigin', 'anonymous');
         $(img).error(function() { console.log("Sorry, image provided doesn't work"); });
         img.onload = function() {
@@ -29,7 +25,7 @@ function handleImage(e) {
             ctx.drawImage(img, 0, 0, 800, 800);
             img2.src = overlay[$("#otype").find('input').index($("#otype").find(':checked'))];
         };
-         img.src = event.target.result;
+        img.src = event.target.result;
     };
     reader.readAsDataURL(e.target.files[0]);
 }
@@ -37,17 +33,14 @@ function handleImage(e) {
 //Page Load
 $(document).ready(function() {
 
-$("#otype").on("change",function(){
-if(imgloaded === true)
-{
-    ctx.drawImage(img, 0, 0, 800, 800);
-    img2.src = overlay[$("#otype").find('input').index($("#otype").find(':checked'))];
-}
+    $("#otype").on("change", function() {
+        if (imgloaded === true) {
+            ctx.drawImage(img, 0, 0, 800, 800);
+            img2.src = overlay[$("#otype").find('input').index($("#otype").find(':checked'))];
+        }
 
+    });
 
-});
-
-  
     c = document.getElementById("maincanvas");
     ctx = c.getContext("2d");
 
@@ -56,12 +49,11 @@ if(imgloaded === true)
         $("#output").empty();
         $("#output").append('<img src="' + c.toDataURL() + '">');
         $("#output").fadeIn("fast").css("display", "block");
-        $("#dlImage").fadeIn("fast").css("display","inline-block");
+        $("#dlImage").fadeIn("fast").css("display", "inline-block");
 
     };
     var imageLoader = document.getElementById('fileupload');
-    imageLoader.addEventListener('change', handleImage, false);
-    
+    imageLoader.addEventListener('change', loadImage, false);
 
     $("#dlImage").on("click", function() {
         console.log("go");
